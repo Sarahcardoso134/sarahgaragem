6649e27032fbb7c2fdab3dbe421dfd1f4137e2d0
--global <--global>
2023-05-15 17:04:07 -0300

    Criação da model veículo

--- garagem/models.py
+++ garagem/models.py
@@ -18,6 +18,10 @@ class Acessorio(models.Model):
 
     def __str__(self):
         return self.descricao
+
+    class Meta:
+        verbose_name = "Acessório"
+
     
 
 class Cor(models.Model):
@@ -25,3 +29,20 @@ class Cor(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+    class Meta:
+        verbose_name_plural = "Cores"
+    
+    
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    ano = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.marca} ({self.categoria}, {self.ano}, {self.cor})"
+    
+    class Meta:
+        verbose_name = "Veículo"
