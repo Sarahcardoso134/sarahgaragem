114084becabe1b7ddbce18f247bc58ddc51fd28b
--global <--global>
2023-05-15 17:16:42 -0300

    Criação API veículos

--- config/urls.py
+++ config/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
-from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet, AcessorioViewSet
-
+from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet, AcessorioViewSet, VeiculoViewSet 
 
 router = routers.DefaultRouter()
-router.register(r"marcas", MarcaViewSet)
+router.register(r"acessórios", AcessorioViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"cores", CorViewSet)
-router.register(r"acessórios", AcessorioViewSet)
+router.register(r"marcas", MarcaViewSet)
+router.register(r"veiculos", VeiculoViewSet)
 
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include(router.urls))]
--- garagem/serializers.py
+++ garagem/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from garagem.models import Marca, Categoria, Cor, Acessorio
+from garagem.models import Marca, Categoria, Cor, Acessorio, Veiculo
 
 
 class MarcaSerializer(ModelSerializer):
@@ -22,3 +22,21 @@ class AcessorioSerializer(ModelSerializer):
     class Meta:
         model = Acessorio
         fields = "__all__"
+
+
+class VeiculoSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
+
+class VeiculoDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
+        depth = 1
+
+
+class VeiculoListSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = ["categoria", "marca", "ano"]
